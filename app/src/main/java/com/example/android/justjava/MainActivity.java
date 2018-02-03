feb016c2ba6757a05f85c158bd17e1b041646726
chilinoodle <mazen426@gmail.com>
2018-02-03 19:57:22 +0100

    added toggleButtons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 import java.text.NumberFormat;
 
 /**
@@ -13,7 +14,10 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
+    int sugar_quantity = 0;
+    int milk_quantity = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        if (quantity > 1) {
+            quantity = quantity - 1;
+        }
+        display(quantity);
+    }
+
+    public void includeSugar(View view) {
+        boolean isChecked = ((ToggleButton) view).isChecked();
+        if (isChecked) {
+            sugar_quantity = quantity;
+        } else {
+            sugar_quantity = 0;
+        }
+    }
+
+    public void includeMilk(View view) {
+        boolean isChecked = ((ToggleButton) view).isChecked();
+        if (isChecked) {
+            milk_quantity = quantity;
+        } else {
+            milk_quantity = 0;
+        }
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
+
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int total = 0;
+        total = quantity * 5 + sugar_quantity + milk_quantity;
+        displayPrice(total);
     }
 
     /**
@@ -44,14 +82,6 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-    public void increment(View view) {
-        quantity = quantity + 1;
-        display(quantity);
-    }
 
-    public void decrement(View view) {
-        quantity = quantity - 1;
-        display(quantity);
-    }
 
 }
